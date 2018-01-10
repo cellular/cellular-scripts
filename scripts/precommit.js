a088c8c2abb153e54d08be56b18744c0886c1181
Felix Gnass <fgnass@gmail.com>
2018-01-10 18:09:46 +0100

    fix lint-staged integration

--- scripts/precommit.js
+++ scripts/precommit.js
@@ -1,5 +1,4 @@
-const path = require('path');
-const resolve = require('resolve');
+const lintStaged = require('lint-staged/src');
 const { hasPkgProp, hasFile } = require('../lib/app');
 
 module.exports = function() {
@@ -8,27 +7,6 @@ module.exports = function() {
     !hasFile('lint-staged.config.js') &&
     !hasPkgProp('lint-staged');
 
-  const lintStagedPath = require.resolve('lint-staged');
-  const cosmiconfigPath = resolve.sync('cosmiconfig', {
-    basedir: path.dirname(lintStagedPath)
-  });
-
-  // lint-staged uses cosmiconfig to find its configuration
-  // and it has no other way to provide config
-  // (via a node API or command-line flag)
-  // So, we're doing this require cache magic to provide our own
-  // config so folks don't have to have that in their package.json
-  function fakeCosmiconfig(...args) {
-    if (args[0] === 'lint-staged') {
-      return Promise.resolve({ config: require('../config/lintstagedrc') });
-    } else {
-      return require(cosmiconfigPath)(...args);
-    }
-  }
-
-  if (useBuiltinConfig) {
-    require.cache[cosmiconfigPath] = { exports: fakeCosmiconfig };
-  }
-
-  require(lintStagedPath);
+  const config = require.resolve('../config/lintstagedrc');
+  lintStaged(console, useBuiltinConfig && config);
 };
