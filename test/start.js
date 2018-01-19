847857471dff4306feb08c8be36a88012b872c5e
Felix Gnass <fgnass@gmail.com>
2018-01-19 15:02:05 +0100

    increase timeout

--- test/start.js
+++ test/start.js
@@ -5,28 +5,32 @@ const spawn = require('cross-spawn');
 
 const bin = require.resolve('../bin/cellular-scripts');
 
-test('start', () => {
-  const appPath = path.join(__dirname, 'fixture', 'builtins');
-  process.chdir(appPath);
-  return portfinder.getPortPromise().then(port => {
-    return new Promise((resolve, reject) => {
-      const child = spawn(bin, ['start', '--port', port], {
-        stdio: ['pipe', 'pipe', 'pipe', 'ipc'],
-        detached: true
-      });
-      child.on('error', reject);
-      child.stdout.once('data', () => {
-        request(`http://localhost:${port}`)
-          .then(res => {
-            process.kill(-child.pid);
-            expect(res).toMatch(/<div id="root">/);
-            resolve();
-          })
-          .catch(err => {
-            process.kill(-child.pid);
-            reject(err);
-          });
+test(
+  'start',
+  () => {
+    const appPath = path.join(__dirname, 'fixture', 'builtins');
+    process.chdir(appPath);
+    return portfinder.getPortPromise().then(port => {
+      return new Promise((resolve, reject) => {
+        const child = spawn(bin, ['start', '--port', port], {
+          stdio: ['pipe', 'pipe', 'pipe', 'ipc'],
+          detached: true
+        });
+        child.on('error', reject);
+        child.stdout.once('data', () => {
+          request(`http://localhost:${port}`)
+            .then(res => {
+              process.kill(-child.pid);
+              expect(res).toMatch(/<div id="root">/);
+              resolve();
+            })
+            .catch(err => {
+              process.kill(-child.pid);
+              reject(err);
+            });
+        });
       });
     });
-  });
-});
+  },
+  60000
+);
