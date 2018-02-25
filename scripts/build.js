441ff4824a0c263a54bacfe2ca14a4e936a5b189
Felix Gnass <fgnass@gmail.com>
2018-02-25 22:19:33 +0100

    pass through args

--- scripts/build.js
+++ scripts/build.js
@@ -3,10 +3,10 @@
 const app = require('about-this-app');
 const spawn = require('spawn-bin');
 
-module.exports = function() {
-  const args = ['--env.prod'];
+module.exports = function(...args /*: string[] */) {
+  const cliArgs = [...args, '--env.prod'];
   if (!app.hasFile('webpack.config.js')) {
-    args.push('--config', require.resolve('../webpack'));
+    cliArgs.push('--config', require.resolve('../webpack'));
   }
-  return spawn('webpack', args).status;
+  return spawn('webpack', cliArgs).status;
 };
